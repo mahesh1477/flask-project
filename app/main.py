b806987e19dafa5e54fc578f93d4cc75c8dbdea1
Mahesh Misal <misalmahesh0505@gmail.com>
2025-11-10 12:11:40 +0530

    Added Procfile and requirements.txt

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home_view():
-        return "<h1>Welcome to Geeks for Geeks</h1>"
+@app.route('/')
+def home():
+    return "Hello, Flask on Render!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
